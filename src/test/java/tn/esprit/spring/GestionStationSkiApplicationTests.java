2ca0f50bee074a881e02b83b730030495eb04416
Lassad BENTALEBALI <lassad.bentalebali@esprit.tn>
2024-11-07 02:50:04 +0100

    njareb3

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -2,9 +2,13 @@ package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import tn.esprit.spring.services.InstructorServicesImpl;
 
-@SpringBootTest
+@SpringBootTest(classes = InstructorServicesImpl.class)
 class GestionStationSkiApplicationTests {
+	@MockBean
+	private InstructorServicesImpl instructorServices;
 
 	@Test
 	void contextLoads() {
